d85dfb413d23028bbfc4fad3bde3efdc463cfb3a
vbhosal1 <vbhosal1@binghamton.edu>
2020-08-02 23:42:20 -0400

    Implemented SentenceDecorator.

--- textdecorators/src/textdecorators/SentenceDecorator.java
+++ textdecorators/src/textdecorators/SentenceDecorator.java
@@ -5,6 +5,7 @@ import textdecorators.util.InputDetailsI;
 public class SentenceDecorator extends AbstractTextDecorator {
     private AbstractTextDecorator atd;
     private InputDetailsI id;
+    private int index = 0;
 
     public SentenceDecorator(AbstractTextDecorator atdIn, InputDetailsI idIn) {
         atd = atdIn;
@@ -14,6 +15,15 @@ public class SentenceDecorator extends AbstractTextDecorator {
     @Override
     public void processInputDetails() {
         // TODO Auto-generated method stub
+        for (String sentence : id.getInputLineList()) {
+            String[] word = sentence.split("\\s");
+
+            word[0] = "BEGIN_SENTENCE__" + word[0];
+            word[word.length - 1] = word[word.length - 1] + "__END_SENTENCE.";
+            id.update(String.join(" ", word), index);
+            index += 1;
+
+        }
         if (null != atd) {
             atd.processInputDetails();
         }
